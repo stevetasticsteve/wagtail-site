ed6cf5ba3eff6b4010b809664ed3996936c8a288
steve <stevetasticsteve@gmail.com>
2021-03-07 20:23:17 +0000

    Set some trial secure settings

--- willowsun/settings/production.py
+++ willowsun/settings/production.py
@@ -10,6 +10,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 MEDIA_URL = os.path.join('media', os.environ.get('SITE_NAME', '') + '/')
 
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_HSTS_SECONDS = 3600
+
 DATABASES = {
     "default": {
         "ENGINE": 'django.db.backends.postgresql_psycopg2',
