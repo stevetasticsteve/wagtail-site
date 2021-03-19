4b3f704dcc5bf631ac34c30983d2dc1cceb42785
steve <stevetasticsteve@gmail.com>
2021-03-19 09:26:29 +0000

    Created a page series

--- home/models.py
+++ home/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from wagtail.core.models import Page
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel, StreamFieldPanel, MultiFieldPanel
-
 from . import blocks
 
 
@@ -76,3 +75,62 @@ class GeneralPage(Page):
     promote_panels = [
         MultiFieldPanel(promote_panels, "Common page configuration")
     ]
+
+
+class SeriesIndexPage(Page):
+    """
+    A page to act as a container for a series. Can include an overview of the series. Links to child pages automatically shown.
+    """
+    parent_page_types = ["GeneralPage"]
+    body = blocks.full_streamfield
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel("body"),
+    ]
+
+    def get_context(self, request, *args, **kwargs):
+        """Adding custom stuff to our context."""
+        context = super().get_context(request, *args, **kwargs)
+
+        context["child_pages"] = self.get_children().live()
+        return context
+
+
+class SeriesPage(Page):
+    """
+    A flexible page for varied content that automatically links to sibling pages belonging to the same family. Designed for creating a series of pages on the same subject.
+    """
+    parent_page_types = ["SeriesIndexPage"]
+
+    summary = models.CharField(
+        max_length=250,
+        help_text="A short summary to go into links.",
+        blank=False,
+        null=False,
+    )
+    body = blocks.full_streamfield
+
+    comments = models.BooleanField(
+        default=True,
+        help_text="Should comments be allowed on the page?",
+        verbose_name="Enable comments?"
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("summary"),
+        StreamFieldPanel("body"),
+    ]
+    promote_panels = Page.promote_panels + [
+        FieldPanel("comments")
+    ]
+
+    promote_panels = [
+        MultiFieldPanel(promote_panels, "Common page configuration")
+    ]
+
+    def get_context(self, request, *args, **kwargs):
+        """Adding custom stuff to our context."""
+        context = super().get_context(request, *args, **kwargs)
+        context["related_pages"] = self.get_siblings().live().specific()
+
+        return context
